52bf5f576498a131917095c5426013a311781883
Abdullah Adil <abdullahadil143@gmail.com>
2025-05-22 18:37:41 +0000

    Created config file

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_HOST',       '8ijr5.h.filess.io');
+define('DB_USER',       'cosc4806_blackthan');
+define('DB_PASS',       $_ENV['DB_PASS']);
+define('DB_DATABASE',   'cosc4806_blackthan');
+define('DB_PORT',       '61000');
+?>
